271571a06aad17482e4cd7ed7b70cdcf13ba6f70
Elias Miralles <elias.miralles@gmail.com>
2018-12-14 15:37:13 +0100

    get detail cashgame

--- routes/cashGames.js
+++ routes/cashGames.js
@@ -4,6 +4,17 @@ const router = express.Router();
 // const User = require('../models/user');
 const CashGame = require('../models/cashGame');
 
+router.get('/:id', (req, res, next) => {
+  const { id } = req.params;
+
+  CashGame.findById(id)
+    .then((game) => {
+      res.status(200);
+      res.json(game);
+    })
+    .catch(next);
+});
+
 router.post('/create', (req, res, next) => {
   const { playerList, pot, isPlaying, owner } = req.body;
 
